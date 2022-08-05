c425f456708034695950d453c04980925d2dc4ed
ThaisMK <thais.konishi@gmail.com>
2022-08-05 19:16:19 -0300

    criando metodo DELETE cliente pelo id

--- src/main/java/com/example/cliente/controller/ClientesController.java
+++ src/main/java/com/example/cliente/controller/ClientesController.java
@@ -9,6 +9,7 @@ import org.apache.catalina.connector.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -54,6 +55,13 @@ public class ClientesController {
 
         URI uri = uriBuilder.path("/client/{id}").buildAndExpand(cliente.getId()).toUri();
         return ResponseEntity.created(uri).body(new ClienteDto(cliente));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity deletar(@PathVariable Long id) {
+        clienteRepository.deleteById(id);
+
+        return ResponseEntity.ok().build();
     }
 
 }
